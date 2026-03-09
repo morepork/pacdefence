3d8880ef42b0707101a7ab8fe73ac371bc15efe3
Liam <pacdefence@gmail.com>
2026-03-10 11:25:16 +1300

    Optimisation

--- logic/Formulae.java
+++ logic/Formulae.java
@@ -13,7 +13,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with Pac Defence.  If not, see <http://www.gnu.org/licenses/>.
- *  
+ *
  *  (C) Liam Byrne, 2008 - 2012.
  */
 
@@ -27,13 +27,11 @@ import towers.Tower.Attribute;
 
 
 public class Formulae {
-   
-   private final static List<Double> moneyDivisors = new ArrayList<Double>();
-   
+
    public static int numCreeps(int level) {
       return 20 + 4 * (level - 1);
    }
-   
+
    public static int ticksBetweenAddCreep(int level) {
       int ticks = 40;
       for(int i = 1; i < level; i++) {
@@ -41,52 +39,47 @@ public class Formulae {
       }
       return ticks > 1 ? ticks : 1;
    }
-   
+
    public static long hp(int level) {
       return (long)(Math.pow(1.5, level - 1) * 10);
    }
-   
+
    public static int levelEndBonus(int level) {
       return 1000 + 100 * (level - 1);
    }
-   
+
    public static int noEnemiesThroughBonus(int level) {
       return level * 100;
    }
-   
+
    public static long upgradeCost(int currentLevel) {
       return (long)(Math.pow(1.25, currentLevel - 1) * 100);
    }
-   
+
    public static double damageDollars(double hpLost, double hpFactor, int level) {
       return hpFactor * hpLost / getMoneyDivisor(level);
    }
-   
+
    public static double killBonus(long levelHP, int level) {
       return levelHP / getMoneyDivisor(level);
    }
-   
-   private static synchronized double getMoneyDivisor(int level) {
-      // Keep a list of these as this is called every time a bullet hits.
-      level--;
-      for(int i = moneyDivisors.size(); i <= level; i++) {
-         moneyDivisors.add(Math.pow(1.15, i));
-      }
-      return moneyDivisors.get(level);
+
+   private static double getMoneyDivisor(int level) {
+      return Math.pow(1.15, level - 1);
    }
-   
+
    public static int nextUpgradeKills(int currentLevel) {
       return currentLevel * currentLevel * 10;
    }
-   
+
    public static long nextUpgradeDamage(int currentLevel) {
       return (long)(Math.pow(2, currentLevel - 1)) * 250;
    }
-   
+
    public static int towerCost(int numTowers, int numOfThisType) {
       return (int)(Math.pow(1.05, numTowers) * Math.pow(1.1, numOfThisType) * 1000);
    }
-   
+
    public static long sellValue(Tower t, int numTowers, int numOfThisType) {
       double value = towerCost(numTowers - 1, numOfThisType - 1);
       for(Attribute a : Attribute.values()) {
